5734280baa45962ac411d7160561cc549132714e
Jon Kristensen <jon@jonkr.dk>
2022-10-31 19:37:11 +0100

    Add project files.

---
+++ BookApp/Data/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BookApp.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+        public string? Publisher { get; set; }
+        public double Price { get; set; }
+        public string ISBN { get; set; }
+        public string Category { get; set; }
+        public DateTime Created { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id={Id}, Title={Title}, Author={Author}, NumberOfPages={NumberOfPages}, Publisher={Publisher}, Price={Price}, ISBN={ISBN}, Category={Category}, Created={Created}";
+        }
+    }
+}
---
+++ BookApp/Data/BookDataDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookApp.Data
+{
+    public class BookDataDbContext : DbContext
+    {
+        public BookDataDbContext(DbContextOptions<BookDataDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
