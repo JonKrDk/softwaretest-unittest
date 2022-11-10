16d0edd57666a36025b49894a88093f01195ef89
Jon Kristensen <jon@jonkr.dk>
2022-11-10 14:08:59 +0100

    NUnit Test added

--- TestBooksController/UnitTest1.cs
+++ TestBooksController/UnitTest1.cs
@@ -1,3 +1,10 @@
+using BookApp.Controllers;
+using BookApp.Data;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+
 namespace TestBooksController
 {
     public class Tests
@@ -8,9 +15,43 @@ namespace TestBooksController
         }
 
         [Test]
-        public void Test1()
+        public void IndexTest()
         {
-            Assert.Pass();
+            var options = new DbContextOptionsBuilder<BookDataDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDb")
+                .Options;
+            var context = new BookDataDbContext(options);
+            var logger = new NullLogger<BooksController>();
+
+            BooksController booksController = new BooksController(context, logger);
+
+            var bookA = new Book()
+            {
+                Title = "Title Number 1",
+                Author = "Author Number 1",
+                NumberOfPages = 117,
+                Publisher = "Publisher Number 1",
+                Price = 25.95,
+                ISBN = "1234567890123",
+                Category = "Category Number 1",
+                Created = new DateTime(year:2022, month: 6, day: 15, hour: 17, minute: 43, second: 34),
+            };
+
+            var result = booksController.Create(bookA);
+
+            result.Should().BeOfType<Task<IActionResult>>();
+
+            var bookB = context.Books.First();
+
+            Assert.That(bookB.Id, Is.EqualTo(1));
+            Assert.That(bookB.Title, Is.EqualTo("Title Number 1"));
+            Assert.That(bookB.Author, Is.EqualTo("Author Number 1"));
+            Assert.That(bookB.NumberOfPages, Is.EqualTo(117));
+            Assert.That(bookB.Publisher, Is.EqualTo("Publisher Number 1"));
+            Assert.That(bookB.Price, Is.EqualTo(25.95));
+            Assert.That(bookB.ISBN, Is.EqualTo("1234567890123"));
+            Assert.That(bookB.Category, Is.EqualTo("Category Number 1"));
+            Assert.That(bookB.Created, Is.EqualTo(new DateTime(year: 2022, month: 6, day: 15, hour: 17, minute: 43, second: 34)));
         }
     }
 }
